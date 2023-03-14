4e4b4aa4c2989f7b2b912dce8b3e3bab38f77f52
Kagan Kaca <79793667+kagankaca@users.noreply.github.com>
2023-03-14 12:27:41 -0700

    Update lab2.cpp

--- lab2/lab2.cpp
+++ lab2/lab2.cpp
@@ -29,6 +29,31 @@ std::string words[1000] = {"a","ability","able","about","above","accept","accord
        }
        number +=1;
     }
+    
+    
+    int n;
+
+    cout << "Please enter a number between 3 and 9: ";
+    cin >> n;
+
+    if (n < 3 || n > 9) {
+        cout << "Invalid input. Please enter a number between 3 and 9." << endl;
+        return 0;
+    }
+
+    for (int i = 1; i <= n; i++) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    for (int i = n-1; i >= 1; i--) {
+        for (int j = 1; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+    }
 
     return 0;
 }
